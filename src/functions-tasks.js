e033dda17a6d5260801bce248666aba9759762f2
Ania Chebysheva <anna.chebysheva@gmail.com>
2024-03-22 03:45:55 +0100

    feat: complete retry task

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -130,8 +130,18 @@ function memoize(/* func */) {
  * }, 2);
  * retryer() => 2
  */
-function retry(/* func, attempts */) {
-  throw new Error('Not implemented');
+function retry(func, attempts) {
+  return () => {
+    let count = 0;
+    while (count < attempts) {
+      try {
+        return func();
+      } catch {
+        count += 1;
+      }
+    }
+    return 0;
+  };
 }
 
 /**
