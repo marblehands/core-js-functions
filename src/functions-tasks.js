820ac30e684a8cebbe137db07176071edd0681f4
Ania Chebysheva <anna.chebysheva@gmail.com>
2024-03-22 19:06:56 +0100

    feat: complete partialUsingArguments task

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -217,8 +217,10 @@ function logger(func, logFunc) {
  *   partialUsingArguments(fn, 'a','b','c')('d') => 'abcd'
  *   partialUsingArguments(fn, 'a','b','c','d')() => 'abcd'
  */
-function partialUsingArguments(/* fn, ...args1 */) {
-  throw new Error('Not implemented');
+function partialUsingArguments(fn, ...args1) {
+  return function curry(...arg) {
+    return fn(...args1, ...arg);
+  };
 }
 
 /**
