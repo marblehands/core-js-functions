d2ccc3481ed91d74d222586abc1e16257c2e185d
Ania Chebysheva <anna.chebysheva@gmail.com>
2024-03-22 03:02:07 +0100

    feat: complete getArgumentsCount task

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -54,8 +54,10 @@ function getFunctionBody(func) {
  *  ]) => [0, 1, 2]
  *
  */
-function getArgumentsCount(/* funcs */) {
-  throw new Error('Not implemented');
+function getArgumentsCount(funcs) {
+  return Array([...funcs].length)
+    .fill(0)
+    .map((item, index) => item + index);
 }
 
 /**
